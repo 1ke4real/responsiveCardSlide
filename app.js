83b5265386c1dd1ed2902f5a74024bdd6c849bf6
Mike Leman <mikeeleman@gmail.com>
2022-10-14 23:49:39 +0200

    installing slide js and styling cards

---
+++ app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlides: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
